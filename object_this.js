8f0944f73c050b30386c20c35915414b0654ced7
Kan <kantippayamontri@gmail.com>
2020-03-10 18:02:16 +0700

    object and this

---
+++ object_this.js
@@ -0,0 +1,19 @@
+console.log(`--- Javascript Objects and Keyword This ---`);
+const user = {
+  name: "first",
+  age: 22,
+  purchases: ["phone", "motorcycle", "laptop"],
+  sayname: function saymyname() {
+    console.log(`My name is ${this.name}`);
+  }
+};
+
+function apple() {
+  console.log("apple");
+}
+console.log(this);
+window.apple();
+this.apple(); //this is window object
+//this.user.saymyname();
+console.log(user.name);
+this.apple();
