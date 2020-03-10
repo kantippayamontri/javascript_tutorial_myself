7f6fbc7cb18151c7113d3be7fce5a5de2478f14b
Kan <kantippayamontri@gmail.com>
2020-03-10 15:39:07 +0700

    start if else

---
+++ app.js
@@ -0,0 +1,61 @@
+//console.log("Hey Kan Tippayamontri");
+//todo old way variable
+// var life = 100;
+// life = life + 10;
+// life = life / 10.0;
+// console.log(life);
+// var name = "first";
+// console.log(name);
+// var checkout = false;
+// var box = null;
+
+//todo new way variable
+// let newlife = 10;
+// const newname = "first eiei";
+
+//todo ------------------ ep 3 ------------------
+// const name = "first";
+// //logger();
+// //console.log(name);
+// //adder(2, 3.2);
+
+// function logger() {
+//   console.log("Party Time");
+//   console.log("Party Time");
+//   console.log("Party Time");
+//   console.log("Party Time");
+// }
+
+// function adder(num1, num2) {
+//   console.log(num1 + num2);
+// }
+// const toUpper = txt => {
+//   const upper = txt.toUpperCase();
+//   console.log(upper);
+// };
+
+// const sum = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// const one = () => {
+//   return 1;
+// };
+
+// toUpper("tipmontri");
+// console.log(sum(4, 8));
+// console.log(one());
+
+//todo ----------------------------------- ep4 -------------------------------
+// console.log("it's me kan");
+// console.log("it's me tipp");
+// let name = "first";
+// let age = 21;
+// console.log(`it's me and my name is ${name} and my age is ${age}`);
+// console.log("it's me and my name is " + name + " and my age is " + age);
+
+// let combind = name + age;
+// console.log(typeof combind);
+// console.log(typeof age);
+
+//todo ----------------------------------ep5 Javascript If Else Statements ----------------------
